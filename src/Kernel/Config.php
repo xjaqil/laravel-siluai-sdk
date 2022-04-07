60ee7fa86832bb5908a04485b4fb049c8d932ddb
ablimit <xjaqil@163.com>
2022-04-07 12:16:40 +0800

    fixed some bugs

--- src/Kernel/Config.php
+++ src/Kernel/Config.php
@@ -5,11 +5,7 @@ namespace Aqil\SiluAi\Kernel;
 
 use EasyWeChat\Kernel\Support\Collection;
 
-/**
- * Class Config.
- *
- * @author overtrue <i@overtrue.me>
- */
+
 class Config extends Collection
 {
 }
